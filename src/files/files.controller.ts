4846fb0078c6d40638ef830d62bc7bce76d94496
Dung Tran <admin@192.168.1.14.non-exists.ptr.local>
2025-05-18 16:38:02 +0700

    add logic upload file

---
+++ src/files/files.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFiles,
+  ParseFilePipeBuilder,
+  HttpStatus,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { UpdateFileDto } from './dto/update-file.dto';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { ResponseMessage } from '@/decorator/customize';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('upload')
+  @ResponseMessage('Upload file successfully')
+  @UseInterceptors(FilesInterceptor('test'))
+  uploadFile(
+    @UploadedFiles(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /^image\/.*$/,
+        })
+        .addMaxSizeValidator({
+          maxSize: 3000 * 1024,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return {
+      fileName: file[0].filename,
+    };
+  }
+
+  @Get()
+  findAll() {
+    return this.filesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.filesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
+    return this.filesService.update(+id, updateFileDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.filesService.remove(+id);
+  }
+}
